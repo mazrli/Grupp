82108c38a32c4aa230a5026239beb8f34cab46af
William With <baconandminecraft@gmail.com>
2021-12-06 12:12:35 +0100

    added intell project

---
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/sjukhus/person/employee/Receptionist.java
@@ -0,0 +1,10 @@
+package se.sahlgrenska.sjukhus.person.employee;
+
+import se.sahlgrenska.sjukhus.item.Order;
+
+import java.util.List;
+
+public class Receptionist extends Employee {
+    private List<Order> orders;
+    Accessibility accessibility;
+}
