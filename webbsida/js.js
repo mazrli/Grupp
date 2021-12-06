ad3a973387a88bed0cbdb9a0c1911f316230ae62
mazrli <91855982+mazrli@users.noreply.github.com>
2021-12-06 12:25:45 +0100

    start
    start sida

---
+++ webbsida/js.js
@@ -0,0 +1,76 @@
+function setFormMessage(formElement, typ, message){
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+
+
+}
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+    loginForm.addEventListener("submit", e => {
+
+        e.preventDefault();
+
+        setFormMessage(loginForm, "error", "Invalid username/password")
+    });
+});
+
+
+function validate(){
+    var user = document.getElementById("user").value;
+    var password = document.getElementById("password").value;
+    if (user == "Admin" && password== "user"){
+        
+        window.location.href="./menu.html";
+        return false;
+        
+
+    }
+ 
+    else{
+        alert("Login is false and Not Regisstered")
+        return true;
+        
+    }
+
+
+
+    
+
+   
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
