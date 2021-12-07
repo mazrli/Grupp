9cd83d6729e273083fbfd4bd5acb246f5a73a489
SlushiestTurtle <92742743+SlushiestTurtle@users.noreply.github.com>
2021-12-07 22:24:10 +0100

    Update
    Update

--- Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Journal/Journal.java
+++
@@ -1,4 +0,0 @@
-package se.sahlgrenska.gui.Journal;
-
-public class Journal {
-}
---
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Journal/JournalGUI.java
@@ -0,0 +1,22 @@
+package se.sahlgrenska.gui.Journal;
+import javas.swing.*;
+
+import javax.swing.*;
+
+public class JournalGUI {
+
+
+JPanel MainPanel;
+JPanel TopPanel;
+JLabel JournalTitleLabel;
+JPanel LeftPanel;
+JPanel DataListPanel;
+JPanel ButtonPanel;
+JPanel CommentPanel;
+JPanel InputPanel;
+    private JLabel JournalDataLabel;
+    private JList JournalDataList;
+    private JLabel DiseaseDataLabel;
+    private JList DiseaseDataList;
+
+}
