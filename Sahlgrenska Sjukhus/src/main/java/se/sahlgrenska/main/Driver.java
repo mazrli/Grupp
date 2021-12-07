60de537f0d637f78248989d2e9493adc0641fee6
william with <william.with@hotmail.se>
2021-12-07 11:19:02 +0100

    init driver class

---
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/main/Driver.java
@@ -0,0 +1,15 @@
+package se.sahlgrenska.main;
+
+import se.sahlgrenska.sjukhus.Hospital;
+import se.sahlgrenska.sjukhus.person.employee.Employee;
+
+public class Driver {
+
+    private Employee currentUser;
+    private Hospital hospital;
+
+    public static void main(String[] args) {
+
+    }
+
+}
