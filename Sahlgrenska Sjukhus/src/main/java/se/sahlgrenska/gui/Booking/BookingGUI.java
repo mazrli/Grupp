3e0de012faada895be0ea8061d45b3d650000a7c
PhoebeMell <43949179+PhoebeMell@users.noreply.github.com>
2021-12-08 12:00:53 +0100

    BookingsGUI

--- Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Booking/BookingGUI.java
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Booking/BookingGUI.java
@@ -1,9 +1,11 @@
 package se.sahlgrenska.gui.Booking;
+
+import se.sahlgrenska.gui.util.HelperGUI;
 import se.sahlgrenska.sjukhus.Booking;
 
 import javax.swing.*;
 
-public class BookingGUI extends JFrame {
+public class BookingGUI extends HelperGUI {
     private Booking booking;
     private JPanel mainPanel;
     private JPanel test;
@@ -11,17 +13,16 @@ public class BookingGUI extends JFrame {
     private JLabel title;
 
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         new BookingGUI().setVisible(true);
     }
 
 
-public BookingGUI(){
-super("Create Booking");
+    public BookingGUI() {
+        init(mainPanel, "Create Booking");
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
-
-}
-
+    }
 
 
 }
