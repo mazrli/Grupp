d76c27678e429d4ab38308d152d10bdfe79c65a3
William With <baconandminecraft@gmail.com>
2021-12-08 21:42:41 +0100

    merge confligt försök 2

---
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Booking/BookingGUI.java
@@ -0,0 +1,14 @@
+package se.sahlgrenska.gui.Booking;
+import se.sahlgrenska.sjukhus.Booking;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BookingGUI {
+    private Booking booking;
+
+    private JPanel mainPanel;
+    private JPanel test;
+    private JPanel testomg;
+
+}
