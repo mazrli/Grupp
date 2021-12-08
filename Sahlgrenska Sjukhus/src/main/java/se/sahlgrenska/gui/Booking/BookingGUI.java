d94617e37ee8c62badfbacf31a905d0d021753ff
PhoebeMell <43949179+PhoebeMell@users.noreply.github.com>
2021-12-08 11:40:01 +0100

    Changes in booking+bookinggui

--- Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Booking/BookingGUI.java
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Booking/BookingGUI.java
@@ -3,11 +3,25 @@ import se.sahlgrenska.sjukhus.Booking;
 
 import javax.swing.*;
 
-public class BookingGUI {
+public class BookingGUI extends JFrame {
     private Booking booking;
-
     private JPanel mainPanel;
     private JPanel test;
     private JPanel testomg;
+    private JLabel title;
+
+
+    public static void main(String [] args) {
+        new BookingGUI().setVisible(true);
+    }
+
+
+public BookingGUI(){
+super("Create Booking");
+
+
+}
+
+
 
 }
