b7621d61ecf31fb15b35025a096286b2e0e3bc96
PhoebeMell <43949179+PhoebeMell@users.noreply.github.com>
2021-12-08 12:42:19 +0100

    BookingStuff

--- Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Booking/BookingGUI.java
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Booking/BookingGUI.java
@@ -4,6 +4,9 @@ import se.sahlgrenska.gui.util.HelperGUI;
 import se.sahlgrenska.sjukhus.Booking;
 
 import javax.swing.*;
+import java.awt.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class BookingGUI extends HelperGUI {
     private Booking booking;
@@ -11,6 +14,10 @@ public class BookingGUI extends HelperGUI {
     private JPanel test;
     private JPanel testomg;
     private JLabel title;
+    private LocalDateTime date;
+
+    private int minWindowSize = 500;
+    private int maxWindowSize = 500;
 
 
     public static void main(String[] args) {
@@ -20,7 +27,9 @@ public class BookingGUI extends HelperGUI {
 
     public BookingGUI() {
         init(mainPanel, "Create Booking");
-        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        this.setDefaultCloseOperation(HIDE_ON_CLOSE);
+     //   setResizable(false);
+
 
     }
 
