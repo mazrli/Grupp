63bfaec4e4ae282cde69e4e6a5420db17a60dfce
PhoebeMell <43949179+PhoebeMell@users.noreply.github.com>
2021-12-12 18:59:10 +0100

    implementering av bookings + items pill

--- Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/sjukhus/Room.java
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/sjukhus/Room.java
@@ -1,6 +1,26 @@
 package se.sahlgrenska.sjukhus;
 
+import java.util.ArrayList;
+import se.sahlgrenska.sjukhus.item.Item;
 public class Room {
     private String name;
     private int size;
+    private ArrayList<Item> itemsInRoom = new ArrayList<Item>();
+
+
+
+
+
+
+
+
+
+    public ArrayList GetItems() {
+
+        return itemsInRoom;
+    }
+
+
+
+
 }
