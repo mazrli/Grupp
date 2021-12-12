61c4dd1de074a23c54e73c2be3874ea7fec104fe
PhoebeMell <43949179+PhoebeMell@users.noreply.github.com>
2021-12-12 19:24:19 +0100

    room and item stuff

--- Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/sjukhus/Room.java
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/sjukhus/Room.java
@@ -1,15 +1,21 @@
 package se.sahlgrenska.sjukhus;
 
 import java.util.ArrayList;
+
+import se.sahlgrenska.sjukhus.item.Equipment;
 import se.sahlgrenska.sjukhus.item.Item;
+
 public class Room {
     private String name;
     private int size;
     private ArrayList<Item> itemsInRoom = new ArrayList<Item>();
 
 
+    private void addItemsToRoom(){
 
+    new Equipment("Defibrilator", "Starts hearts", 50.5f, true),new Equipment("MRI", "Scans body", 50.0f, true));
 
+}
 
 
 
@@ -19,8 +25,6 @@ public class Room {
 
         return itemsInRoom;
     }
-
-
 
 
 }
