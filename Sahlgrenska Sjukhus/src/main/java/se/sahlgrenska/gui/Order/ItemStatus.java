91ca4381c226571c2e5804c4d250af1596693984
mazrli <abdulaziz.almazrli@elev.nbi-handelsakademin.se>
2021-12-12 18:58:29 +0100

    order
    order

--- Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Order/ItemStatus.java
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Order/ItemStatus.java
@@ -1,14 +1,66 @@
 package se.sahlgrenska.gui.Order;
 
 import se.sahlgrenska.gui.util.HelperGUI;
+import se.sahlgrenska.main.Driver;
+import se.sahlgrenska.sjukhus.person.employee.Accessibility;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ItemStatus extends HelperGUI {
+
+
+
+
+
+public class ItemStatus<panel1> extends HelperGUI {
 
     private JPanel panel1;
     private JTextField textField1;
     private JFormattedTextField formattedTextField2;
+    private JButton Cancel;
+    private JButton Send;
 
 
+
+    JPanel ItemStatus;
+
+
+    public ItemStatus() {
+        this.panel1 = panel1;
+
+
+
+        init(panel1, "Item Status", new Dimension(700, 700),Accessibility.RECEPTIONIST);
+
+
+
+
+
+
+        Cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                Driver.getMainMenu().setVisible(true);
+
+            }
+        });
+
+        Send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                Driver.getMainMenu().setVisible(true);
+                new OrderGUI();
+            }
+        });
+
+
+
+
+
+
+    }
 }
---
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/Order/button.java
@@ -0,0 +1,41 @@
+package se.sahlgrenska.gui.Order;
+
+import se.sahlgrenska.gui.util.HelperGUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class button extends HelperGUI implements ActionListener {
+
+    JButton button;
+    OrderGUI OrderGUI = new OrderGUI();
+
+
+
+
+    button(){
+
+        JButton button = new JButton();
+        button.setBounds(200, 100, 100, 50 );
+        button.setHorizontalTextPosition();
+        button.setVerticalTextPosition(JButton.BOTTOM);
+
+
+        button.addActionListener(e -> System.out.println("poo") );
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500 ,500);
+        this.setVisible(true);
+        this.add(button);
+        this.setFocusable(false);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button){
+            System.out.println("poo");
+        }
+    }
+}
