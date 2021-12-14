c11261ad0d8b150d728cd3d23c916cd015def935
William With <baconandminecraft@gmail.com>
2021-12-14 15:10:51 +0100

    Skapade patient login gui

--- Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/LogIn/LoginPatientGUI.java
+++ Sahlgrenska Sjukhus/src/main/java/se/sahlgrenska/gui/LogIn/LoginPatientGUI.java
@@ -3,10 +3,13 @@ package se.sahlgrenska.gui.LogIn;
 import se.sahlgrenska.gui.util.HelperGUI;
 import se.sahlgrenska.gui.util.misc.SuggestionDropDownDecorator;
 import se.sahlgrenska.gui.util.misc.TextComponentSuggestionClient;
+import se.sahlgrenska.main.Driver;
 import se.sahlgrenska.sjukhus.person.employee.Accessibility;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,12 +20,23 @@ public class LoginPatientGUI extends HelperGUI {
     private JPanel mainPanel;
     private JTextField textField1;
     private JButton okButton;
+    private LogInGUI logInGUI;
 
     public LoginPatientGUI() {
-        init(mainPanel, "Patient", new Dimension(150, 100), Accessibility.PATIENT);
+        init(mainPanel, "Patient", new Dimension(250, 150), Accessibility.PATIENT);
         setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+                Driver.getLoginGUI().setVisible(true);
+            }
+        });
 
     }
+
+
+
+
 
 
 }
